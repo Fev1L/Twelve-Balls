a14d5610bec154261ae6168e0fc2f6245da2b484
Fev1L <demchvlad17@gmail.com>
2025-03-05 18:09:51 +0100

    Logic Complete

--- Jeremi/main.cpp
+++ Jeremi/main.cpp
@@ -10,16 +10,65 @@
 using namespace std;
 int normalBall = 2;
 int lightBall = 1;
-int hardBall = 3;
+int heavyBall = 3;
 
 string game(){
     int arr[12] = {2,2,2,2,2,2,2,2,2,2,2,2};
-    int l = rand() & 12;
-    int h = rand() & 12;
+    int l = rand() % 11;
+    int h = rand() % 11;
     
+    int x,y,z;
+    
+    int lChoise, hChoise;
+    
+    bool win = false;
+    if(h == l){
+        do{
+            h = rand() & 11;
+        }while(h != l);
+    }
+    
+    
+    cout<<"h: "<<h<<"l: "<<l<<endl;
+    
+    for(int i = 0;i<12;i++){
+        if(i == l){
+            arr[i] = 1;
+        }
+        if(i == h){
+            arr[i] = 3;
+        }
+    }
+    
+    for(int i = 0;i<12;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    do{
+        cout<<"Please chose first ball [From 0 - 11]: "<<endl;
+        cin>>x;
+        cin>>y;
+        cin>>z;
+        cout<<"Averege masa is: "<<arr[x] + arr[y] + arr[z]<<endl;
+        cout<<"Chose light ball (you can chose randomly if you dont know): "<<endl;
+        cin>>lChoise;
+        cout<<"Chose heavy ball (you can chose randomly if you dont know): "<<endl;
+        cin>>hChoise;
+        if(lChoise == l && hChoise == h){
+            win = true;
+        }else if(lChoise == l && hChoise != h){
+            cout<<"light ball is correct"<<endl;
+        }else if(lChoise != l && hChoise == h){
+            cout<<"heavy ball is correct"<<endl;
+        }else{
+            cout<<"Nice try"<<endl;
+        }
+    }while(win != true);
     
     return "You are win!";
 }
+
+
 
 int main(int argc, const char * argv[]) {
     srand(time(0));
