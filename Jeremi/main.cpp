5ecb2b318f2f961697513873d91bb0b53467570a
Fev1L <demchvlad17@gmail.com>
2025-03-05 16:09:47 +0100

    Initial Commit

---
+++ Jeremi/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Jeremi
+//
+//  Created by Fev1L on 05.03.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
