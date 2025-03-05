f505e5735eb170fa14c50a19bbae9719831acd63
Fev1L <demchvlad17@gmail.com>
2025-03-05 18:46:57 +0100

    Full Version

--- Jeremi/main.cpp
+++ Jeremi/main.cpp
@@ -8,28 +8,63 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-int normalBall = 2;
-int lightBall = 1;
-int heavyBall = 3;
 
-string game(){
-    int arr[12] = {2,2,2,2,2,2,2,2,2,2,2,2};
-    int l = rand() % 11;
-    int h = rand() % 11;
+int l = rand() % 11;
+int h = rand() % 11;
+
+class twelwBalls{
+private:
+    
     
     int x,y,z;
     
     int lChoise, hChoise;
     
     bool win = false;
+public:
+    void endGame(){
+        cout<<"Chose light ball (you can chose randomly if you dont know): "<<endl;
+        cin>>lChoise;
+        cout<<"Chose heavy ball (you can chose randomly if you dont know): "<<endl;
+        cin>>hChoise;
+        
+        if(lChoise == l && hChoise == h){
+            cout<<"You are win";
+            exit(0);
+        }else if(lChoise == l && hChoise != h){
+            cout<<"light ball is correct"<<endl;
+            exit(0);
+        }else if(lChoise != l && hChoise == h){
+            cout<<"heavy ball is correct"<<endl;
+            exit(0);
+        }else{
+            cout<<"Nice try"<<endl;
+            exit(0);
+        }
+    }
+    void balls(int arr[12]){
+        cout<<"Please chose three balls [From 0 - 11]: "<<endl;
+        cin>>x;
+        cin>>y;
+        cin>>z;
+        if(x != y && x != z && y != z){
+            cout<<"Averege masa is: "<<arr[x] + arr[y] + arr[z]<<endl;
+        }else{
+            cout<<"Dont cheat! Chose not similar"<<endl;
+        }
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    bool end = false;
+    string answear;
+    srand(time_t(0));
+    int arr[12] = {2,2,2,2,2,2,2,2,2,2,2,2};
     if(h == l){
         do{
             h = rand() & 11;
         }while(h != l);
     }
-    
-    
-    cout<<"h: "<<h<<"l: "<<l<<endl;
     
     for(int i = 0;i<12;i++){
         if(i == l){
@@ -39,39 +74,18 @@ string game(){
             arr[i] = 3;
         }
     }
-    
-    for(int i = 0;i<12;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
+    twelwBalls obj;
     do{
-        cout<<"Please chose first ball [From 0 - 11]: "<<endl;
-        cin>>x;
-        cin>>y;
-        cin>>z;
-        cout<<"Averege masa is: "<<arr[x] + arr[y] + arr[z]<<endl;
-        cout<<"Chose light ball (you can chose randomly if you dont know): "<<endl;
-        cin>>lChoise;
-        cout<<"Chose heavy ball (you can chose randomly if you dont know): "<<endl;
-        cin>>hChoise;
-        if(lChoise == l && hChoise == h){
-            win = true;
-        }else if(lChoise == l && hChoise != h){
-            cout<<"light ball is correct"<<endl;
-        }else if(lChoise != l && hChoise == h){
-            cout<<"heavy ball is correct"<<endl;
-        }else{
-            cout<<"Nice try"<<endl;
+        cout<<"Chose comands[stop,balls,chose]: "<<endl;
+        cin>>answear;
+        if(answear == "stop"){
+            end = true;
+            exit(0);
+        }else if(answear == "chose"){
+            obj.endGame();
+        }else if(answear == "balls"){
+            obj.balls(arr);
         }
-    }while(win != true);
-    
-    return "You are win!";
-}
-
-
-
-int main(int argc, const char * argv[]) {
-    srand(time(0));
-    cout<<game()<<endl;
+    }while(end == false);
     return 0;
 }
