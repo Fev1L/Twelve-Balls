8e5413e6d363cd468d6a04d606bf447983117e15
Fev1L <demchvlad17@gmail.com>
2025-03-05 17:13:58 +0100

    MacOS 12 update

--- Jeremi/main.cpp
+++ Jeremi/main.cpp
@@ -8,15 +8,21 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-
+int normalBall = 2;
+int lightBall = 1;
+int hardBall = 3;
 
 string game(){
+    int arr[12] = {2,2,2,2,2,2,2,2,2,2,2,2};
+    int l = rand() & 12;
+    int h = rand() & 12;
     
     
     return "You are win!";
 }
 
 int main(int argc, const char * argv[]) {
+    srand(time(0));
     cout<<game()<<endl;
     return 0;
 }
